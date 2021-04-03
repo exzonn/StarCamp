b52dd239c386c7b07a89d99fb89357116dd9a7ec
Mykola <kolyaglega@gmail.com>
2021-04-03 16:46:39 -0400

    basic styles added

--- seeds/index.js
+++ seeds/index.js
@@ -13,20 +13,24 @@ db.once("open", () => {
     console.log("Database connected");
 })
 
-const sample = arr => arr[ Math.floor(Math.random() * arr.length) ];
+const sample = arr => arr[Math.floor(Math.random() * arr.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(places)} ${sample(descriptors)}`
+            title: `${sample(places)} ${sample(descriptors)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.Beatae porro animi commodi accusantium laborum, eaque nobis, eum distinctio laboriosam similique possimus repellendus et doloremque fugiat non ratione sunt officia iste!',
+            price 
         })
         await camp.save();
     }
 }
 
-seedDB().then(()=>{
+seedDB().then(() => {
     mongoose.connection.close();
 })
