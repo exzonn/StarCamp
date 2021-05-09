c9126c4fec4a60ea6af37ad6bac66376c5a2cf5f
Mykola <kolyaglega@gmail.com>
2021-05-09 15:55:58 -0400

    seed camps

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
-mongoose.connect('mongodb://localhost:27017/starcamp', {
+const dbUrl = process.env.DB_URL
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
